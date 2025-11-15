40965e6f60c1ce1dc78620726fbac2e332010bd6
Linahamza <lina94.hamza@gmail.com>
2025-11-15 22:28:29 +0100

    docs: fix badges and add comprehensive documentation
    - Fix CI/CD badge URL (actions/workflows format)
    - Add Docker Hub badges (size, pulls)
    - Add technology stack badges
    - Improve documentation structure

--- src/test/java/com/devops/myshop/MyshopApplicationTests.java
+++ src/test/java/com/devops/myshop/MyshopApplicationTests.java
@@ -2,7 +2,7 @@ package com.devops.myshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+// test basique (contextLoads)
 @SpringBootTest
 class MyshopApplicationTests {
 
